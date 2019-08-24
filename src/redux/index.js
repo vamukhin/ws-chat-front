eb15ec53680bbb683f2022587c4b36cc38259e5c
mkhnvld <va.mukhin@physics.msu.ru>
2019-08-24 15:31:03 +0300

    heroku fix

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,12 +1,11 @@
 import { createStore, applyMiddleware  } from 'redux'
-import thunk from 'redux-thunk';
 import logger from 'redux-logger'
 import { socketMiddleware } from './socket-middleware';
 import { socketHandlers } from './socket-handlers';
 import { rootReducer } from './reducers';
 import { fromSocketToReduxHandlers } from './from-socket-ro-redux-thunks';
 
-const middlewares = [socketMiddleware("ws://localhost:8081", socketHandlers, fromSocketToReduxHandlers), thunk];
+const middlewares = [socketMiddleware("ws://wschat-server.herokuapp.com/:8081", socketHandlers, fromSocketToReduxHandlers)];
 
 if (process.env.NODE_ENV !== 'production') { 
     middlewares.push(logger)
